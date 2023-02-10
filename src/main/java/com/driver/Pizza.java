1fb3a2afe364895ef324596f03f02823b17db3c7
Ankit Litouriya <ankitlitouriya8719@gmail.com>
2023-02-11 00:47:56 +0530

    first solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,30 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
+    private int toppings;
     private String bill;
+    boolean  ischeesadded;
+    boolean istoppings;
+    boolean isbiilCreated;
+    boolean istakeAway;
+
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+         if (isVeg)
+         {
+          this.price =300;
+          this.toppings =70;
+         }
+         else {
+             this.price=400;
+             this.toppings=120;
+         }
+         this.bill = "Base Price Of The Pizza:"+this.price +"\n";
+
     }
 
     public int getPrice(){
@@ -17,18 +36,48 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!ischeesadded) {
+            this.price += 80;
+            this.ischeesadded = true;
+        }
+
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!istoppings){
+            this.price+=this.toppings;
+            this.istoppings= true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!istakeAway)
+        {
+            this.price+=20;
+           this.istakeAway=true;
+        }
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
-        return this.bill;
+        if (isbiilCreated) {
+            if (ischeesadded) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            }
+            if (istoppings) {
+                this.bill += "Extra Toppings Added:" + this.toppings + "\n";
+            }
+            if (istakeAway) {
+                this.bill += "Paperbag Added:" + "\n";
+            }
+            this.bill += "Total Price:" + this.price;
+            return this.bill;
+        }
+        return "";
     }
+
+
 }
